d0a2fad684b16f61dc4e501eb0946b9d2dcd458a
kawaribeiro2310 <kawaribeiroalves@gmail.com>
2026-05-23 10:53:46 -0300

    media valores

---
+++ MediaValores.py
@@ -0,0 +1,17 @@
+qnt= 0
+soma= 0
+media= 0
+valor = float(input("Digite um valor:"))
+
+while (valor > 0.0):
+    soma = soma + valor
+    qnt = qnt + 1
+    #Entrada de valores
+    valor = float(input("Digite um valor:"))
+
+#caso digite um valor negativo o laço encerra
+
+media = soma / qnt
+print("\n Total da soma:", soma)
+print("\n Quantidade de valores digitados:", qnt)
+print("\n Média dos valores:", media) 
